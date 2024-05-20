226cfee4b7992d719e802fcea07172b2717b2462
Marco Tam <46859133+mad-hin@users.noreply.github.com>
2024-05-20 22:53:59 +0800

    Update load.sh

--- tc_q/load.sh
+++ tc_q/load.sh
@@ -1,1 +1,3 @@
-sudo TC_LIB_DIR='./tc' tc qdisc add dev enp0s3 root marco_fq 
+cd iproute2
+sudo TC_LIB_DIR='./tc' tc qdisc add dev veth0 root marco_fq limit 100 & sudo TC_LIB_DIR='./tc' tc qdisc add dev enp0s3 root marco_fq limit 100
+tc qdisc list
